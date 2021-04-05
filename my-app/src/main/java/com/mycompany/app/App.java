e6abd0ce7e4c0c0fae923d845f5e133add4a93c4
omprakash K <kalagoni.omprakash@gmail.com>
2021-04-05 04:27:46 +0200

    updated app.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Jenkins worked kudos!! Keep Working!! Success will come to you!! Face any problem BE HARD!!!!" );
+        System.out.println( "Hello World! Jenkins worked kudos !! Updated by developer" );
     }
 }
